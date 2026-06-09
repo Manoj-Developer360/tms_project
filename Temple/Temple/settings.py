9f7d0108ffdb6b489b9d9be8133cc40c844c1a8e
Manoj45code <manobala45678@gmail.com>
2026-06-09 12:05:42 +0530

    Fix static files configuration

--- Temple/Temple/settings.py
+++ Temple/Temple/settings.py
@@ -114,8 +114,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
+# Static files
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'Templeapp' / 'static',
+]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-
