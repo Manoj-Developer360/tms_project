d43a4c60e4deca2196bfd1bef2f490da9af00362
Manoj45code <manobala45678@gmail.com>
2026-06-09 11:55:58 +0530

    Fix root URL routing

--- Temple/Templeapp/urls.py
+++ Temple/Templeapp/urls.py
@@ -19,6 +19,7 @@ from django.urls import include,path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('home',views.home,name='home'),
     path('login',views.login,name='login'),
     path('otp',views.otp,name='otp'),
